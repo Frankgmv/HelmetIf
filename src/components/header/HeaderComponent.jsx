4cb3a22cb52df2853e96b88dca62cc3673b258f5
Frankgmv <giovanyvelasquez2004@gmail.com>
2024-04-13 01:04:37 -0500

    ajustes

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -9,7 +9,7 @@ const HeaderComponent = () => {
                 <div className="banner-fondo"></div>
                 <div className="banner-content">
                     <div className="banner-title">SENA dev&apos;s </div>
-                    <p className="banner-descripcion">Portafolio de habilidades de desarrollares del sitio oficial del colegio I. E. Centenario de Pereira, desde
+                    <p className="banner-descripcion">Portafolio de habilidades de desarrollares del sitio oficial del colegio I. E. Centenario Pereira, desde
                         datos personales hasta profesionales. &nbsp;
                         Acompañame a conocer más sobre el talento humano que llevo desarrollar el sitio que conecta una bonita comunidad.
                     </p>

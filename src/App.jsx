7e14e9a7ac0628918bdd46412d8ff8618a3d0de4
Frankgmv <giovanyvelasquez2004@gmail.com>
2024-04-13 01:23:48 -0500

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HashRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import "toastr/build/toastr.css";
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/*" element={<HomePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 

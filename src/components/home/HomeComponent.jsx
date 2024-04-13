d024fea77fb00b2a1ccbdee8b0a1f6edd4fdd81a
Frankgmv <giovanyvelasquez2004@gmail.com>
2024-04-13 13:03:19 -0500

    ajustes

--- src/components/home/HomeComponent.jsx
+++ src/components/home/HomeComponent.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import FormularioComponent from "../formulario/FormularioComponent"
 
 const HomeComponent = () => {
@@ -9,8 +10,8 @@ const HomeComponent = () => {
             <div className="stack-tech" id="stack-tech">
                 <h3>Conoce los creadores</h3>
                 <ul className="container-tech">
-                    <li className="tech"><a href="./frank"> Frank</a></li>
-                    <li className="tech"><a href="./kevin"> Kevin</a></li>
+                    <li className="tech"><Link href="./frank"> Frank</Link></li>
+                    <li className="tech"><Link href="./kevin"> Kevin</Link></li>
                 </ul>
             </div>
             <div className="stack-tech" style={{fontSize: '.7em'}} id="stack-tech">
